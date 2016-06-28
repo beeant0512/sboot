457193d2824941f1f75a1a3c4cf085f984d05f19
Beeant <huangrongbiao@sina.com>
2016-06-28 15:29:25 +0800

    remove extend

--- src/sboot.button.js
+++ src/sboot.button.js
@@ -1,19 +1,19 @@
-(function($, window, document, undefined) {
+(function ($, window, document, undefined) {
     'use strict';
-    $.fn.button = function(parameters) {
+    $.fn.button = function (parameters) {
         var elements = $(this),
             invokeReturn,
             args = arguments,
-            el = elements.each(function(index, element) {
+            el = elements.each(function (index, element) {
                 var $this = $(this),
                     data = $(this).data('sboot.button'),
-                    queryValues = parameters == 'extend' ? data.queryValues : [].slice.call(args, 1),
+                    queryValues = [].slice.call(args, 1),
                     methodInvoked = (typeof parameters === 'string');
                 if (!data) {
                     $this.data('sboot.button', (data = new Button(this, parameters, queryValues)))
                 } else {
                     data.queryValues = queryValues;
-                };
+                }
                 if (methodInvoked) {
                     invokeReturn = data.invoke(parameters);
                 }
@@ -25,59 +25,38 @@
         return el;
     };
 
-    var Button = function(element, settings, queryValues) {
+    var Button = function (element, settings, queryValues) {
         this.$element = element;
-        this.settings = ($.isPlainObject(settings)) ? $.extend(true, {}, Button.settings, settings) : $.extend({}, Button.settings),
-            this.queryValues = queryValues;
+        this.settings = ($.isPlainObject(settings)) ? $.extend(true, {}, Button.settings, settings) : $.extend({}, Button.settings);
     };
 
     Button.settings = {
         count: 60,
         template: '%s',
-        click: function() {}
+        click: function () {}
+    };
+
+    Button.prototype.toggle = function(){
+
     };
 
     Button.prototype.set = {
-        text: function(queryValues) {
+        text: function (queryValues) {
             console.log('set text');
             this.$element.innerHTML = queryValues;
         },
-        settings: function(settings) {
+        settings: function (settings) {
             console.log('set settings');
             this.settings = $.extend(this.settings, settings[0]);
         }
     };
 
-    Button.prototype.extend = function(parameters) {
-        console.log('extend');
-        var functionName = parameters[0],
-            callback = parameters[1],
-            functions = functionName.split(" ");
-        var depth = functions.length;
-        if (depth > 1) {
-            if ($.isFunction(this[functions[0]][functions[1]])) {
-                this[functions[0]][functions[1]].call(this, this.queryValues);
-                callback(this.queryValues);
-            }
-        } else {
-            if ($.isFunction(this[functions[0]])) {
-                this[functions[0]].call(this, this.queryValues);
-                callback(this.queryValues);
-            };
-        }
-
-        if ($.isFunction(this[functionName])) {
-            this[functionName].call(this, this.queryValues);
-            callback(this.queryValues);
-        }
-    }
-
     Button.prototype.get = {
-        text: function() {
+        text: function () {
             console.log('get text');
             return this.innerHTML;
         },
-        settings: function(queryValues) {
+        settings: function (queryValues) {
             console.log('get settings');
             if ($.isFunction(queryValues[0])) {
                 queryValues[0](this.settings);
@@ -87,19 +66,20 @@
         }
     };
 
-    Button.prototype.invoke = function(parameters) {
+    Button.prototype.invoke = function (parameters) {
         console.log('invoke');
         parameters = parameters.split(" ");
         var depth = parameters.length;
         if (depth > 1) {
             if ($.isFunction(this[parameters[0]][parameters[1]])) {
-                return this[parameters[0]][parameters[1]].call(this, this.queryValues);
+                return this[parameters[0]][parameters[1]].call(this, this.data('sboot.button').queryValues);
             }
         } else {
             if ($.isFunction(this[parameters[0]])) {
-                return this[parameters[0]].call(this, this.queryValues);
+                return this[parameters[0]].call(this, this.data('sboot.button').queryValues);
 
-            };
+            }
+            ;
         }
 
     }
